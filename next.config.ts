0c24ba8c1b15f9f21c33f36daa97449744aa0c03
faeemaxp <naeemansari962n@gmail.com>
2026-03-24 20:37:21 +0530

    path fix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const isProd = process.env.NODE_ENV === 'production';
-const basePath = isProd ? '/ExistTV' : '';
+const isVercel = process.env.VERCEL === '1';
+
+// Only use /ExistTV if in production and NOT on Vercel (e.g. for GitHub Pages)
+const basePath = (isProd && !isVercel) ? '/ExistTV' : '';
 
 const nextConfig: NextConfig = {
   output: 'export',
